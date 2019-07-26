1beb7798cd83f3489fd1dcd4d8a0e128ce00df69
Andrea Law <me@andrealaw.co>
2019-07-26 15:46:58 +0800

    MIG-707 Add global settlement savings dai service

--- packages/dai-plugin-migrations/src/MigrationService.js
+++ packages/dai-plugin-migrations/src/MigrationService.js
@@ -1,14 +1,23 @@
 import { PublicService } from '@makerdao/services-core';
 import { ServiceRoles, Migrations } from './constants';
 import SingleToMultiCdp from './migrations/SingleToMultiCdp';
+import GlobalSettlementSavingsDai from './migrations/GlobalSettlementSavingsDai';
 
 const migrations = {
-  [Migrations.SINGLE_TO_MULTI_CDP]: SingleToMultiCdp
+  [Migrations.SINGLE_TO_MULTI_CDP]: SingleToMultiCdp,
+  [Migrations.GLOBAL_SETTLEMENT_SAVINGS_DAI]: GlobalSettlementSavingsDai
 };
 
 export default class MigrationService extends PublicService {
   constructor(name = ServiceRoles.MIGRATION) {
-    super(name, ['smartContract', 'accounts', 'cdp', 'proxy']);
+    super(name, [
+      'smartContract',
+      'accounts',
+      'cdp',
+      'proxy',
+      'mcd:globalSettlement',
+      'mcd:savings'
+    ]);
   }
 
   getAllMigrationsIds() {
--- packages/dai-plugin-migrations/test/MigrationService.spec.js
+++ packages/dai-plugin-migrations/test/MigrationService.spec.js
@@ -1,6 +1,7 @@
 import { migrationMaker } from './helpers';
 import { ServiceRoles, Migrations } from '../src/constants';
 import SingleToMultiCdp from '../src/migrations/SingleToMultiCdp';
+import GlobalSettlementSavingsDai from '../src/migrations/GlobalSettlementSavingsDai';
 
 let maker, service;
 
@@ -15,16 +16,20 @@ describe('Migration Service', () => {
 
     expect(ids).toEqual(
       expect.arrayContaining([
-        Migrations.SINGLE_TO_MULTI_CDP
+        Migrations.SINGLE_TO_MULTI_CDP,
+        Migrations.GLOBAL_SETTLEMENT_SAVINGS_DAI
       ])
     );
-    expect(ids.length).toEqual(1);
+    expect(ids.length).toEqual(2);
   });
 
   test('getting each migration returns a valid migration', () => {
     expect(service.getMigration(Migrations.SINGLE_TO_MULTI_CDP)).toBeInstanceOf(
       SingleToMultiCdp
     );
+    expect(
+      service.getMigration(Migrations.GLOBAL_SETTLEMENT_SAVINGS_DAI)
+    ).toBeInstanceOf(GlobalSettlementSavingsDai);
   });
 
   test('getting a non-existent migration returns undefined', () => {
